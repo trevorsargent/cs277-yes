78b05791078be39d71ea3f76d1f65700d5f37a1b
Trevor Sargent <tbird1710@gmail.com>
2015-04-16 21:33:45 -0700

    main

--- driver.c
+++ driver.c
@@ -5,13 +5,16 @@
 
 int main(int argc, char *argv[]){
 
-	//see notes.md for structure of chip array
+	// see notes.md for structure of chip array
 	int* chip = malloc(sizeof(int) * 12);
 
-	//main memory
+	// main memory
 	unsigned char* memory = malloc(sizeof(char) * pow(2,19));
 
+	// read in file
 	int stackBottom = readFile(argc, argv, memory);
+
+	//something else
 
 	return 0;
 }
